52b6907329b4c63c1e35f9bed10189f0058fbaeb
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-10 14:55:39 +0000

    added successful solution to tickett 03

--- 03-star-slider-challenge/app.js
+++ 03-star-slider-challenge/app.js
@@ -24,16 +24,26 @@ let starIcon = document.querySelector(".star-icon");
 
 //3.attempt to change the value using js
 
-let newVariable = document.querySelector(".star-icon").innerHTML="newtext";
+// let newVariable = document.querySelector(".star-icon").innerHTML="newtext";
 
 //4. link the slider to the newVariable
 
 function replaceStars(event) {
-
-  starIcon.innerHTML = 
+  if (event.target.value === "1"){
+  newStars = "★"
+  } else if (event.target.value === "2") {
+    newStars = "★★"
+  } else if (event.target.value === "3") {
+    newStars = "★★★"
+  } else if (event.target.value === "4") {
+    newStars = "★★★★"
+  } else {newStars = "★★★★★"}
+  starIcon.innerHTML = newStars
 }
 
-let newStars = addEventListener()
+sliderContainer.addEventListener("input", replaceStars);
+
+  // grabColorPicker.addEventListener("input", changeColor);
 
 //function changeColor(event) {
   
@@ -41,8 +51,21 @@ let newStars = addEventListener()
   //grabCanvas.style.backgroundColor = event.target.value;
 //}
 // document.getElementById('id_element_in_html').innerHTML = `<li> ${somejsvar}</li>
-console.log(newVariable);
-//console.log(starIcon);
+// console.log(newVariable);
+// //console.log(starIcon);
 //change the display to a word - test
 
 //can we just access the content of the star-icon
+
+
+//The above works kind of, it replaces the 'star' image with the number from the slider
+//We think we need to use another variabe to equate the value of the slider, to a new number of stars, to then update the webpage
+
+// ★
+// ★★
+// ★★★
+// ★★★★
+// ★★★★★
+
+// if event.target.value = 1,
+// new stars = "★"
