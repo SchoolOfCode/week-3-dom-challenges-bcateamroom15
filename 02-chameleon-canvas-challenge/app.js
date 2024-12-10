a7a07a3dfbf69aee32fa89690c7d4fabb1b6a8a6
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-10 13:17:13 +0000

    Canvas challenge attempted

--- 02-chameleon-canvas-challenge/app.js
+++ 02-chameleon-canvas-challenge/app.js
@@ -13,3 +13,41 @@ Expected Outcomes:
 */
 
 // Place your plan and solution below!
+//1. Grab the canvas object using getElementbyId
+const grabCanvas = document.getElementById("canvas");
+
+//2. Grab the colour from the colour picker as an input
+const grabColorPicker = document.getElementById("colorPicker");
+
+//3. Function which takes colour as input and changes the canvas to that colour
+// we need to access the color property of the canvas
+//change the css property of the .canvas object
+function changeColor(event) {
+  grabColorPicker.addEventListener("input", );
+  // event.target.value
+  grabCanvas.style.backgroundColor = event.target.value
+}
+//4. Call the function with addEventListener
+
+
+// We need to access the css property background-color
+//How do we do this?
+const canvasStyle = getComputedStyle(grabCanvas);
+console.log(canvasStyle);
+console.log(canvasStyle.backgroundColor);
+
+
+//can we log the color property of colorPicker
+// console.log("colorPicker");
+// console.log(grabColorPicker)
+// const colorPickerValue = getComputedStyle(grabColorPicker);
+// console.log(colorPickerValue);
+
+// console.log(addEventListener("input", grabColorPicker));
+// console.log(changeColor())
+
+// console.log(colorPicker.addEventListener("input", grabColorPicker))
+// grabColorPicker.value
+//it's a number!
+
+console.log(changeColor())
---
+++ 02-chameleon-canvas-challenge/olieTest.js
@@ -0,0 +1,8 @@
+//How to log the input of the colorPicker?
+
+let getColor = document.getElementById("colorPicker");
+getColor.addEventListener("change")
+
+console.log|(getColor.value)
+
+
